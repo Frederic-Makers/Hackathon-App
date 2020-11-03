b0c9e4477f2a2b2b65bfab05f37eb95e6165ebef
hiva pothier <hiva pothier@LAPTOP-B0SL40A8>
2020-11-04 10:09:31 +1400

    j'ai fini les class PrestataireAccess avec le  la class DevisPrestataire get/insert/update a l'intérieur j'ai fini l'objet DevisPrestataire

---
+++ PrestataireBusiness/DevisPrestataire.cs
@@ -0,0 +1,18 @@
+﻿namespace PrestataireBusiness
+{
+    public class DevisPrestataire
+    {
+        public int Id { get; set; }
+        public int DevisId { get; set; }
+        public int PrestataireId { get; set; }
+        public int PrixId { get; set; }
+
+        public DevisPrestataire(int id, int devisid, int prestataireid, int prixid)
+        {
+            this.Id = id;
+            this.DevisId = devisid;
+            this.PrestataireId = prestataireid;
+            this.PrixId = prixid;
+        }
+    }
+}
