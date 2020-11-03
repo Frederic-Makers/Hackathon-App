7c2a2faee34878c50575b1c9b2094089f800bfa9
HP-TOUCH <HP-TOUCH@WINDOWS-MAKERS>
2020-11-03 13:23:31 -0930

    New

--- PrestataireBusiness/Prestataire.cs
+++ PrestataireBusiness/Prestataire.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace PrestataireBusiness
 {
+
     class Prestataire
     {
+        public int id { get; set; }
+        public String nom { get; set; }
+        public String url { get; set; }
+        public String categorie { get; set; }
+        public String contact { get; set; }
+        public String adresse { get; set; }
+        public String description { get; set; }
+        public Boolean activation { get; set; }
     }
 }
