94e6e639c0ae8db1ea87d51b94e2fd30edfaf010
hiva pothier <hiva pothier@LAPTOP-B0SL40A8>
2020-11-04 10:20:09 +1400

    MAJ 04/11 10h20

--- PrestataireBusiness/PrestataireAcces.cs
+++ PrestataireBusiness/PrestataireAcces.cs
@@ -80,7 +80,7 @@ namespace PrestataireBusiness
                 cmd.Parameters.AddWithValue("@description", p.Description);
                 cmd.Parameters.AddWithValue("@activation", p.Activation);
                 cmd.Parameters.AddWithValue("@prix", p.Prix);
-                
+
                 bool result = cmd.ExecuteNonQuery() == 1;
                 cmd.Connection.Close();
                 return result;
@@ -110,77 +110,5 @@ namespace PrestataireBusiness
 
             }
         }
-    }
-
-
-
-    public class DevisPrestataireAccess
-    {
-        private static MySqlConnection connection;
-
-        static DevisPrestataireAccess()
-        {
-            String connString = "Server=remotemysql.com/phpmyadmin/sql.php?server=1&db=qgO0M364Or&table=Prestataire&pos=0;Database=qgO0M364Or;userid=sgroot;Pwd=7Hyomgetg3";
-            connection = new MySqlConnection(connString);
-        }
-        public static void GetDevisPrestataire()
-        {
-            String sql = "SELECT * FROM devisprestataire";
-            using (MySqlCommand command = new MySqlCommand(sql, connection))
-            {
-                command.Connection.Open();
-                using (System.Data.Common.DbDataReader dbReader = command.ExecuteReader())
-
-                {
-                    while (dbReader.Read())
-                    {
-                        int id = dbReader.GetInt32(0);
-                        int devisid = dbReader.GetInt32(1);
-                        int prestataireid = dbReader.GetInt32(2);
-                        int prixid = dbReader.GetInt32(3);
-                       
-                        Business.DevisPrestataire.Add(new DevisPrestataire(id, devisid, prestataireid, prixid));
-
-                    }
-                }
-                command.Connection.Close();
-            }
-
-        }
-        public static bool InsertDevisPrestataire(DevisPrestataire p)
-        {
-            string sql = "INSERT INTO prestataire(Id, DevisId, PrestataireId) " +
-                         "VALUE (@id, @devisid, @prestataireid)";
-            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
-            {
-                cmd.Connection.Open();
-                cmd.Parameters.AddWithValue("@id", null);
-                cmd.Parameters.AddWithValue("@devisid", p.DevisId);
-                cmd.Parameters.AddWithValue("@prestataireid", p.PrestataireId);
-               
-                bool result = cmd.ExecuteNonQuery() == 1;
-                cmd.Connection.Close();
-                return result;
-            }
-        }
-        public static bool UpdateDevisPrestataire(DevisPrestataire p)
-        {
-            string sql = "UPDATE devisprestataire SET DevisId=@devisid, PrestataireId=@prestataireid"
-                + " Where Id=@id ";
-
-            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
-            {
-                cmd.Connection.Open();
-
-                cmd.Parameters.AddWithValue("@id", p.Id);
-                cmd.Parameters.AddWithValue("@devisid", p.DevisId);
-                cmd.Parameters.AddWithValue("@prestataireid", p.PrestataireId);
-           
-                bool result = cmd.ExecuteNonQuery() == 1;
-                cmd.Connection.Close();
-                return result;
-
-            }
-        }
-    }
+    } 
 }
