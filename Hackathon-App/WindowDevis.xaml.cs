c20562269cb7bf684d8a447cea2aeae7c7c9dd1e
FALCHETTO Marc <Revton@hotmail.fr>
2020-11-05 15:29:10 -0930

    Last MAJ 05/11/2020

--- Hackathon-App/WindowDevis.xaml.cs
+++ Hackathon-App/WindowDevis.xaml.cs
@@ -92,9 +92,7 @@ namespace Hackathon_App
             wd.Numero.Text = d2.Numero;
 
 
-            wd.LoadPrestataireSelected.ItemsSource = Devis.DevisPrestataires;
-        
-
+            wd.LoadPrestataireSelected.ItemsSource = d.DevisPrestataires;
 
         }
 
